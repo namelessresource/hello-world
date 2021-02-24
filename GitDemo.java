a7f7e1d3ee9fecf3a983d2c378903f53e2be68ed
namelessresource <79564244+namelessresource@users.noreply.github.com>
2021-02-24 00:58:28 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Pooja Dwivedi
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,8 +14,11 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        
         System.out.print("Add num to previousNumber");
-        return 0;
+
+
+        return previousNumber + num;
     }
     
     /*****
@@ -41,6 +45,7 @@ public class GitDemo {
      *****/
     public double reverseNumber(double num){
         double newNumber = 1;
+
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
